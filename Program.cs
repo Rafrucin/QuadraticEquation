2e11256243b805f8a92e374ccfa7363aafd8f63b
Rafał Ruciński <rafal_rucinski@poczta.fm>
2020-04-04 20:19:11 +0100

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+public class QuadraticEquation
+{
+    public static Tuple<double, double> FindRoots(double a, double b, double c)
+    {
+
+
+        double x1, x2;
+        //if (a!=0 && (b*b-c)>0)
+        //{
+            x1 = (-b + Math.Sqrt(b * b - (4*a*c))) / (2 * a);
+            x2 = (-b - Math.Sqrt(b * b - 4*a*c)) / (2 * a); 
+            return Tuple.Create( x1, x2);
+        //}
+    }
+
+    public static void Main(string[] args)
+    {
+        Tuple<double, double> roots = QuadraticEquation.FindRoots(2, 10, 8);
+        Console.WriteLine("Roots: " + roots.Item1 + ", " + roots.Item2);
+    }
+}
